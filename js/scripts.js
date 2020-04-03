483c16e62782755a5c3a96a4a00ca69d5d601a95
Code-Cacophony <57301331+Code-Cacophony@users.noreply.github.com>
2020-04-03 16:53:04 -0700

    be able to turn a single input  integer into seperate numbers

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,10 @@ $(document).ready(function () {
     event.preventDefault();
     var userInput = parseInt($("#sentence").val());
     for (index = 0; index <= userInput; index++) {
-      alert(index);
+      var array = []
+      var newArray = index.toString();
+
+      console.log(newArray);
     }
   });
 });
