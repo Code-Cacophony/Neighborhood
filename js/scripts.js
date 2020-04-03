575a1051ad2498a78790ffd013f766eef15aa2f8
Code-Cacophony <57301331+Code-Cacophony@users.noreply.github.com>
2020-04-03 14:19:03 -0700

    add console.log to display user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,9 @@
-var num = ["1", "2", "3"]
-
-var num2 = num.parseInt();
-
-
-
+var num = ["1", "2", "3"];
 
 $(document).ready(function () {
-  $("").submit(function (event) {
+  $("form#user-input").submit(function (event) {
     event.preventDefault();
-
-
-
+    var userInput = parseInt($("#sentence").val());
+    console.log(userInput);
   });
 });
