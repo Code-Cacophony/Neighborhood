573c038af46f7a738924f156a90821385a7c10e7
Code-Cacophony <57301331+Code-Cacophony@users.noreply.github.com>
2020-04-03 16:31:18 -0700

    Accidedntally break program with an infinite loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 var num = [];
 
-function userInput(input) {
-  for (var i = 0; i < input; i++);
-  console.log(input);
-}
+
 
 $(document).ready(function () {
   $("form#user-input").submit(function (event) {
     event.preventDefault();
     var userInput = parseInt($("#sentence").val());
-    //console.log(userInput);
+    for (index = 0; index <= 4; index += 1) {
+      alert(index);
+    }
   });
 });
