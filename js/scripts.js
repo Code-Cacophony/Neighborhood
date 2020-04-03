adf12c9a465eedcb6477ad538a751012104e4f55
Code-Cacophony <57301331+Code-Cacophony@users.noreply.github.com>
2020-04-03 16:42:51 -0700

    Tame the infinite loop

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   $("form#user-input").submit(function (event) {
     event.preventDefault();
     var userInput = parseInt($("#sentence").val());
-    for (index = 0; index <= 4; index += 1) {
+    for (index = 0; index <= userInput; index++) {
       alert(index);
     }
   });
